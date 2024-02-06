75c943dc0546c0d5dec4a90d56baf1d4b4d625e9
sayantani27 <sayantani.mondal27@gmail.com>
2024-02-06 00:10:36 -0500

    set up of basic Spring Boot Application is done

---
+++ src/main/java/com/upgrad/hirewheels/HireWheelsApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.hirewheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireWheelsApplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireWheelsApplicatonApplication.class, args);
+	}
+
+}
