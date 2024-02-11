e640f4200087ef021ce5bbee92ce7ae9289a0609
sayantani27 <sayantani.mondal27@gmail.com>
2024-02-11 09:54:09 -0500

    updated pom.xml and aoolication.properties file
    added entities
    
    added dao layer

--- src/main/java/com/upgrad/hirewheels/HireWheelsApplicatonApplication.java
+++ src/main/java/com/upgrad/hirewheels/HireWheelsApplicatonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HireWheelsApplicatonApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HireWheelsApplicatonApplication.class, args);
 	}
 
